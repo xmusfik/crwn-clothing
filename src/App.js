f9d81fd6816a480d5f093f00c55de33f96a3a977
Musfikur rahman <xmusfik@gmail.comclear>
2020-10-22 12:07:07 +0600

    redux setup finished and some minor fixes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React,{Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch , Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth , createUserProfileDocument } from './firebase/firebase.utils';
-
-
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
   unsubscribeFromAuth=null;
 
   componentDidMount(){
+   const {setCurrentUser}= this.props;
     this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
          const userRef=await createUserProfileDocument(userAuth);
          userRef.onSnapshot(snapShot=> {
-           this.setState({
-             currentUser:{
+           setCurrentUser({
                id:snapShot.id,
                ...snapShot.data()
-             }
-           })
-         })
+             });
+         });
          
       }else{
-        this.setState({currentUser:userAuth})
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -43,15 +36,22 @@ class App extends Component {
 
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to='/'/>) :(<SignInAndSignUpPage/>) } />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({user}) =>({
+  currentUser: user.currentUser
+})  
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth,signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
     constructor() {
@@ -14,9 +14,15 @@ class SignIn extends Component {
             password: ''
         }
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        const {email,password}=this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '', password: '' })
+        }catch(e){
+            console.log(e);
+        }
     }
     handleChange = event => {
         const { value, name } = event.target;
